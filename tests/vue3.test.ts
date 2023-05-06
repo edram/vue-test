f2947afb4090f58d9db61d0dfdf5803be154bc6f
edram <edram@qq.com>
2023-05-06 11:21:58 +0800

    init

---
+++ tests/vue3.test.ts
@@ -0,0 +1,10 @@
+/// <reference types="vitest" />
+
+import { describe, expect, test } from "vitest";
+
+
+describe('测试 vue3', () => {
+  test("todo", () => {
+    expect(true).eq(true);
+  });
+});
